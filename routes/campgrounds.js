8a5d19be12ba0e0e5d60ae6a72206d0061b51c2c
SanketSrivastava17 <abd.srs45@gmail.com>
2025-03-18 17:35:28 +0530

    edited campground.js,show.ejs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,23 +6,27 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
 const { storage } = require('../cloudinary');
 const upload = multer({ storage });
-
 const Campground = require('../models/campground');
+
+// Load environment variables
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+// Retrieve Mapbox token from .env
+const mapboxToken = process.env.MAPBOX_TOKEN;
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
-
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
     .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
